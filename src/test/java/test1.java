fb1943be36bedced35ccb8f995de5321db8c0600
sermis97 <sermis97@gmail.com>
2021-09-11 22:09:24 +0300

    my second commit

---
+++ src/test/java/test1.java
@@ -0,0 +1,9 @@
+public class test1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("My first project with GIT");
+        System.out.println("I made some change");
+
+    }
+}
